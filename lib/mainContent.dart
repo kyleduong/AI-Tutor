ced30efc732508f31897ee50857f6a7a80a8d63b
kyleduong <kyleduong06@gmail.com>
2025-01-31 14:49:21 -0800

    enter bug fixed

--- lib/mainContent.dart
+++ lib/mainContent.dart
@@ -1,4 +1,6 @@
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class MainContent extends StatefulWidget{
   @override
@@ -6,8 +8,53 @@ class MainContent extends StatefulWidget{
 }
 
 class _mainContentState extends State<MainContent>{
-  String chat = "";
+  String chat = "Start typing to see text here..."; // Initial text
+  final TextEditingController _controller = TextEditingController(); // Text controller
+  final FocusNode _textFieldFocus = FocusNode(); // Focus Node to access keyboard events
+
   int _selectedChatIndex = 0;
+  bool _shiftPressed = false;
+
+  // This part i Just added
+late final _focusNode = FocusNode(
+  onKeyEvent: (FocusNode node, KeyEvent evt) {
+    if (!HardwareKeyboard.instance.isShiftPressed &&
+        evt.logicalKey.keyLabel == 'Enter') {
+      if (evt is KeyDownEvent) {
+        _performSearch(); // -> implement this
+      }
+      return KeyEventResult.handled;
+
+    } else if (HardwareKeyboard.instance.isShiftPressed &&
+        evt.logicalKey.keyLabel == 'Enter'){
+      _addAutomaticNewLine();
+      return KeyEventResult.handled;
+    } else {
+      return KeyEventResult.ignored;
+    }
+  },
+);
+
+  void _performSearch() {
+      setState((){
+        chat = _controller.text; // update chat with text in field
+        _controller.clear();
+      });
+      print('Search Pressed!');
+    }
+
+  void _addAutomaticNewLine() {
+    setState((){
+        _controller.text += '\n'; // Adds the new line
+    });
+
+  }
+
+  @override
+  void dispose() {
+    _textFieldFocus.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,7 +99,7 @@ class _mainContentState extends State<MainContent>{
                           child: Padding(
                             padding: const EdgeInsets.all(8.0),
                             child: Text(
-                              'No patterns to detect here. jfkdsal;faljdks' * 500,
+                              chat, // this will display the current chat variable
                               style: TextStyle(color: Colors.white),
                             ),
                           ),
@@ -76,6 +123,9 @@ class _mainContentState extends State<MainContent>{
                                 // TextField expands to take leftover horizontal space
                                 Expanded(
                                   child: TextField(
+                                    autofocus: true,
+                                    focusNode: _focusNode,
+                                    controller: _controller, // Used to track input
                                     cursorColor: Colors.black,
                                     style: TextStyle(color: Colors.white),
                                     decoration: InputDecoration(
@@ -93,16 +143,22 @@ class _mainContentState extends State<MainContent>{
                                     keyboardType: TextInputType.multiline,
                                     minLines: 1,  // Minimum height
                                     maxLines: 6, // Allows infinite vertical expansion
+                                    //onSubmitted: (value) => _performSearch(), // Also triggers on Enter
+                                    textInputAction: TextInputAction.none, // so we handle it ourselves
                                   ),
+                                
                                 ),
                                 SizedBox(width: 10),
 
-                                // Left Button
+                                // Search Button
                                 SizedBox(
                                   width: 50,
                                   height: 50,
                                   child: ElevatedButton(
                                     onPressed: () {
+                                      setState((){
+                                        chat = _controller.text; // update chat with text in field
+                                      });
                                       print('Search Pressed!');
                                     },
                                       style: ElevatedButton.styleFrom(
@@ -115,19 +171,166 @@ class _mainContentState extends State<MainContent>{
                                 
                                 SizedBox(width: buttonSpacing),
 
-                                // Right Button
+                                // Reset Chat Button
                                 SizedBox(
                                   width: 50,
                                   height: 50,
                                   child: ElevatedButton(
                                     onPressed: () {
-                                      print('New Chat Pressed!');
+                                      setState(() {
+                                        chat = "Start typing to see text here..."; // Reset text
+                                        _controller.clear(); // Clear input field
+                                      });
+                                      print('Reset Chat Pressed!');
                                     },
                                       style: ElevatedButton.styleFrom(
                                       padding: EdgeInsets.zero, // Removes default padding
                                       alignment: Alignment.center, // Ensures the child is centered
                                     ),
                                     child: Icon(Icons.edit_note_outlined), //Text('New Chat'),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            ),
+                          ),
+                        ),
+                      
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/*
+import 'package:flutter/material.dart';
+
+
+class MainContent extends StatefulWidget {
+  @override
+  _mainContentState createState() => _mainContentState();
+}
+
+class _mainContentState extends State<MainContent> {
+  String chat = "Start typing to see text here..."; // Initial text
+  final TextEditingController _controller = TextEditingController(); // Text controller
+
+  int _selectedChatIndex = 0;
+
+  void _performSearch() {
+      setState((){
+        chat = _controller.text; // update chat with text in field
+      });
+      print('Search Pressed!');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+    final double containerWidth = screenSize.width * 0.7;
+    final double containerHeight = screenSize.height * 0.5;
+    final double sidebarSpace = screenSize.width * 0.1;
+    final double buttonSpacing = screenSize.width * 0.005;
+    final double buttonWidth = screenSize.width * 0.07;
+    final double buttonHeight = screenSize.height * 0.05;
+
+    return Expanded(
+      child: Container(
+        color: Colors.grey[800],
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                width: containerWidth,
+                margin: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                  color: Colors.grey[800],
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: SingleChildScrollView(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              chat,
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey[700],
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Expanded(
+                                  child: TextField(
+                                    controller: _controller,
+                                    cursorColor: Colors.black,
+                                    style: TextStyle(color: Colors.white),
+                                    decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      enabledBorder: InputBorder.none,
+                                      focusedBorder: InputBorder.none,
+                                      hintText: 'Type here...',
+                                      hintStyle: TextStyle(color: Colors.grey[400]),
+                                    ),
+                                    keyboardType: TextInputType.multiline,
+                                    minLines: 1,
+                                    maxLines: 6,
+                                    onSubmitted: (_) { // we are passing _ as we dont need the string
+                                      _performSearch();
+                                    },
+                                  ),
+                                ),
+                                SizedBox(width: 10),
+                                SizedBox(
+                                  width: 50,
+                                  height: 50,
+                                  child: ElevatedButton(
+                                    onPressed: _performSearch,
+                                      style: ElevatedButton.styleFrom(
+                                      padding: EdgeInsets.zero,
+                                      alignment: Alignment.center,
+                                    ),
+                                    child: Icon(Icons.search),
+                                  ),
+                                ),
+                                SizedBox(width: buttonSpacing),
+                                SizedBox(
+                                  width: 50,
+                                  height: 50,
+                                  child: ElevatedButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        chat = "Start typing to see text here...";
+                                        _controller.clear();
+                                      });
+                                      print('Reset Chat Pressed!');
+                                    },
+                                    style: ElevatedButton.styleFrom(
+                                      padding: EdgeInsets.zero,
+                                      alignment: Alignment.center,
+                                    ),
+                                    child: Icon(Icons.edit_note_outlined),
                                   ),
                                 ),
                               ],
@@ -146,3 +349,4 @@ class _mainContentState extends State<MainContent>{
     );
   }
 }
+*/
